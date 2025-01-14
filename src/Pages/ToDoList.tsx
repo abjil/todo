b5be77a2d63d86a64431013b5d2126723748f52f
abjil <abjil2@gmail.com>
2025-01-14 01:48:39 -0800

    moving website layout into components

--- src/Pages/ToDoList.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const ToDoList = () => {
-    return (
-        <span>ToDo List Page</span>
-    );
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import { ToDoList } from './Pages/ToDoList';
+//import './index.scss';
+import './assets/scss/normalize.scss'
+import './assets/scss/style.scss'
+import { ToDoListPage } from './Pages/ToDoListPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ToDoList />
+    <ToDoListPage />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
