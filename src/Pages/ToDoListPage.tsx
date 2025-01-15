7c909bc84ce653f9feafad18b1726b75a2968095
abjil <abjil2@gmail.com>
2025-01-15 12:50:27 -0800

    notifications with Sonner implemented

--- src/Pages/ToDoListPage.tsx
+++ src/Pages/ToDoListPage.tsx
@@ -3,6 +3,7 @@ import { Form } from "../components/Form/Form";
 import { Header } from "../components/Header/Header";
 import { ToDoList } from "../components/ToDoList/ToDoList";
 import { ToDo } from "../models/todo-item";
+import { Toaster, toast } from 'sonner';
 
 export const ToDoListPage = () => {
     const [todos, setTodos] = useState<ToDo[]>([])
@@ -14,6 +15,7 @@ export const ToDoListPage = () => {
             isDone: false
         };
         setTodos([...todos, newToDo]);
+        toast("Task " + text + " is created");
     }
 
     const updateToDo = (toDoItem: ToDo) => {
@@ -24,16 +26,19 @@ export const ToDoListPage = () => {
             return item;
         })
         setTodos(newTodos);
+        toast("Task " + toDoItem.text + " is updated");
     }
 
     const deleteToDo = (toDoItem: ToDo) => {
         const newTodos = todos.filter((todo) => todo.id !== toDoItem.id);
         setTodos(newTodos);
+        toast("Task " + toDoItem.text + " is deleted");
     }
 
     return (
         <>
             <Header />
+            <Toaster position="bottom-right" expand={true} />
             <Form createNewToDo={createNewToDo} />
             <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
         </>
