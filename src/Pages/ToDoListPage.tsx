93891d05cbe345cbd878a1176cfc39cd2cc85712
abjil <abjil2@gmail.com>
2025-01-15 11:53:45 -0800

    todo list logic implemented

--- src/Pages/ToDoListPage.tsx
+++ src/Pages/ToDoListPage.tsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import { Form } from "../components/Form/Form";
 import { Header } from "../components/Header/Header";
 import { ToDoList } from "../components/ToDoList/ToDoList";
+import { ToDo } from "../models/todo-item";
 
 export const ToDoListPage = () => {
+    const [todos, setTodos] = useState<ToDo[]>([])
+
+    const createNewToDo = (text: string) => {
+        const newToDo: ToDo = {
+            id: todos.length,
+            text: text,
+            isDone: false
+        };
+        setTodos([...todos, newToDo]);
+    }
+
+    const updateToDo = (toDoItem: ToDo) => {
+        const newTodos = todos.map((item) => {
+            if (item.id === toDoItem.id) {
+                item.isDone = !item.isDone;
+            }
+            return item;
+        })
+        setTodos(newTodos);
+    }
+
+    const deleteToDo = (toDoItem: ToDo) => {
+        const newTodos = todos.filter((todo) => todo.id !== toDoItem.id);
+        setTodos(newTodos);
+    }
+
     return (
         <>
             <Header />
-            <Form />
-            <ToDoList />
+            <Form createNewToDo={createNewToDo} />
+            <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
         </>
 
     );
