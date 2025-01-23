91a9deaa6cada14df322b10a7c4beb88a13f3e24
abjil <abjil2@gmail.com>
2025-01-22 21:58:33 -0800

    using React Router

--- src/Pages/ToDoListPage.tsx
+++ src/Pages/ToDoListPage.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Form } from "../components/Form/Form";
-import { Header } from "../components/Header/Header";
 import { ToDoList } from "../components/ToDoList/ToDoList";
 import { ToDo } from "../models/todo-item";
 import { Toaster, toast } from 'sonner';
@@ -37,7 +36,6 @@ export const ToDoListPage = () => {
 
     return (
         <>
-            <Header />
             <Toaster position="bottom-right" expand={true} />
             <Form createNewToDo={createNewToDo} />
             <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
